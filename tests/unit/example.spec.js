ef780c07db5a538c1f6ead030a4d3a5d11da345c
mamchurrr <s.mamchurrr@gmail.com>
2019-02-09 10:59:03 +0200

    add 2 tests to Task.vue about rendering input for changing Name of task

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,42 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, createLocalVue  } from '@vue/test-utils'
+import Task from '@/components/tasks/task.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+
+describe('Вид компонента Task', () => {
+  let state
+  let store
+
+  beforeEach(() => {
+    state = {
+      task: {
+        name: "Task Test",
+        description: "Text Task Test",
+      },
+    }
+
+    store = new Vuex.Store({
+      state
     })
-    expect(wrapper.text()).toMatch(msg)
+  })
+
+  it('Не рендерит input для изменения имени таски - без нажатия на Н1', () => {
+    const TaskWrapper = shallowMount(Task, { store, localVue })
+    expect(TaskWrapper.html()).not.toContain('<input>')
+  })
+
+  it('Рендерит input для изменения имени таски после нажатия', () => {
+    const TaskWrapper = shallowMount(Task, { store, localVue })
+    TaskWrapper.setData({
+      changeName: true
+    })
+    expect(TaskWrapper.find('.changer').isVisible()).toBe(
+      true
+    )
   })
 })
